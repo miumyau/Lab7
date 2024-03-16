38b1ab0bec6a8e0e727db3599778eb0303fd0056
ArtiOnMoon <67146003+ArtiOnMoon@users.noreply.github.com>
2024-03-16 15:36:00 +0300

    123

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,10 +1,45 @@
 ﻿namespace Task1;
-
+//Два треугольника заданы длинами своих сторон. Определить, площадь какого из них больше
+//(создать метод для вычисления  площади  треугольника по длинам его сторон).  Для решения задачи можно использовать формулу Герона 
 class Program
 {
+    static double GetSquare (double a, double b, double c)
+    {
+        double p = (double)(a + b + c) / 2;
+        return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Введите стороны первого треугольника");
+        double a1 = Convert.ToDouble(Console.ReadLine());
+        double b1 = Convert.ToDouble(Console.ReadLine());
+        double c1 = Convert.ToDouble(Console.ReadLine());
+        double S1 = GetSquare(a1, b1, c1);
+
+        Console.WriteLine("Введите стороны второго треугольника");
+        double a2 = Convert.ToDouble(Console.ReadLine());
+        double b2 = Convert.ToDouble(Console.ReadLine());
+        double c2 = Convert.ToDouble(Console.ReadLine());
+        double S2 = GetSquare(a2, b2, c2);
+
+       
+        if (S1 > S2)
+        {
+            Console.WriteLine("Площадь первого треугольника больше");
+
+        }
+        else if (S1 < S2)
+        {
+            Console.WriteLine("Площадь второго треугольника больше");
+
+        }
+
+        else
+        {
+            Console.WriteLine("Площадь первого треугольника равна площади второго треугольника ");
+        }
+        Console.ReadKey();
     }
 }
 
---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task2;
+//Создать метод для вычисления объема и площади поверхности куба по длине его ребра.
+class Program
+{
+    static void GetVolSquare (double a, out double V, out double S)
+    {
+        V = a * a * a;
+        S = 6 * a * a;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите длину ребра куба");
+        double a = Convert.ToDouble(Console.ReadLine());
+        double V;
+        double S;
+        GetVolSquare(a, out V, out S);
+        Console.WriteLine("Площадь {0:.00}, Объем {1:.00}",S,V);
+        Console.ReadKey();
+    }
+}
+
